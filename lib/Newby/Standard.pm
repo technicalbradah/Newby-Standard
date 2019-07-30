a252fba54118893b97327714b10a4585842766a5
Jeric O. Newby <tech@newbyan.com>
2019-07-30 19:46:47 -0400

    Initial Commit.

---
+++ lib/Newby/Standard.pm
@@ -0,0 +1,34 @@
+package Newby::Standard;
+# ABSTRACT: Add an abstract here...
+
+use Newby::Standard::Imports;
+
+1;
+
+__END__
+
+=head1 SYNOPSIS
+
+    use Newby::Standard;
+
+=head1 DESCRIPTION
+
+[Describe this module]
+
+=head1 USAGE
+
+[Describe this module's usage]
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<{{$homepage_url}}>.
+
+=head1 SOURCE
+
+Source repository is at L<{{$repository_url}}>.
+
+=head1 BUGS
+
+If you have any suggestions for improvements, or find any bugs, please report them to L<{{$issues_url}}>.  I will be notified, and then you'll automatically be notified of progress on your bug as I make changes.
+
+=cut
